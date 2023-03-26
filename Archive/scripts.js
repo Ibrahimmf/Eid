5554a001e2129803929b8346492d6f3a857d0a7b
Ibrahimmf <ibr1122@gmail.com>
2023-03-26 11:46:27 +0300

    edite the save cards size

--- Archive/scripts.js
+++ Archive/scripts.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     const nameInput = document.getElementById("name-input");
     const submitName = document.getElementById("submit-name");
     const saveCard = document.getElementById("save-card");
     const nameDisplay = document.getElementById("name-display");
 
-    submitName.addEventListener("click", function () {
+    submitName.addEventListener("click", function() {
         const name = nameInput.value;
         if (name.trim() !== "") {
             nameDisplay.textContent = name;
@@ -13,17 +13,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    saveCard.addEventListener("click", function () {
+    saveCard.addEventListener("click", function() {
         if (nameDisplay.textContent.trim() !== "") {
             const cardImage = document.getElementById("card-image");
             const canvas = document.createElement("canvas");
             const ctx = canvas.getContext("2d");
 
-            canvas.width = cardImage.width;
-            canvas.height = cardImage.height;
+            // تعيين حجم الكانفاس بنفس حجم الصورة
+            //canvas.width = cardImage.width;
+            //canvas.height = cardImage.height;
+
+            canvas.width = cardImage.innerWidth;
+            canvas.height = cardImage.innerHeight;
 
             ctx.drawImage(cardImage, 0, 0);
-            ctx.font = "bold 3em Arial";
+
+            // تعيين نسبة الارتفاع إلى العرض
+            const ratio = canvas.width / 500;
+            const fontSize = 60 * ratio;
+            const lineHeight = 80 * ratio;
+
+            ctx.font = `bold ${fontSize}px Arial`;
             ctx.fillStyle = "white";
             ctx.textAlign = "center";
             ctx.textBaseline = "middle";
@@ -31,7 +41,12 @@ document.addEventListener("DOMContentLoaded", function () {
             ctx.shadowOffsetX = 2;
             ctx.shadowOffsetY = 2;
             ctx.shadowBlur = 4;
-            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height * 0.8);
+            ctx.fillText(
+                nameDisplay.textContent,
+                canvas.width / 2,
+                canvas.height * 0.7,
+                canvas.width - 20
+            );
 
             const link = document.createElement("a");
             link.href = canvas.toDataURL("image/png");
