f87c7819016e9c4e2e897780e27855f83159193a
Ibrahimmf <ibr1122@gmail.com>
2023-03-26 17:21:20 +0300

    jav123

--- scripts.js
+++ scripts.js
@@ -1,21 +1,54 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>بطاقات التهنئة</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>أدخل اسمك لإنشاء بطاقة التهنئة</h1>
-        <input type="text" id="nameInput" placeholder="اكتب اسمك هنا">
-        <button onclick="generateCard()">إنشاء بطاقة التهنئة</button>
-        <div class="card">
-            <img src="card_background.jpg" alt="خلفية بطاقة التهنئة" id="cardImage">
-            <div class="card-text" id="cardText"></div>
-        </div>
-    </div>
-    <script src="script.js"></script>
-</body>
-</html>
+document.addEventListener("DOMContentLoaded", function () {
+    const nameInput = document.getElementById("name-input");
+    const submitName = document.getElementById("submit-name");
+    const saveCard = document.getElementById("save-card");
+    const nameDisplay = document.getElementById("name-display");
+
+    const generateCard = function () {
+        const name = nameInput.value;
+
+        if (name.trim() !== "") {
+            nameDisplay.textContent = name;
+
+            const cardText = document.getElementById("card-text");
+            cardText.style.display = "block";
+
+            const cardImage = document.getElementById("card-image");
+            cardImage.style.display = "block";
+        } else {
+            alert("الرجاء إدخال اسم قبل المتابعة.");
+        }
+    };
+
+    saveCard.addEventListener("click", function () {
+        if (nameDisplay.textContent.trim() !== "") {
+            const cardImage = document.getElementById("card-image");
+            const cardText = document.getElementById("card-text");
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            canvas.width = cardImage.offsetWidth;
+            canvas.height = cardImage.offsetHeight;
+
+            ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
+            ctx.font = "bold 3em Arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.shadowColor = "black";
+            ctx.shadowOffsetX = 2;
+            ctx.shadowOffsetY = 2;
+            ctx.shadowBlur = 4;
+            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height * 0.8);
+
+            canvas.toBlob(function (blob) {
+                const link = document.createElement("a");
+                link.href = window.URL.createObjectURL(blob);
+                link.download = "بطاقة_التهنئة.png";
+                link.click();
+            });
+        } else {
+            alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
+        }
+    });
+});
