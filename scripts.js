d0da9633ae279a5a950e5fef9d249169421174ff
Ibrahimmf <ibr1122@gmail.com>
2023-03-26 14:35:31 +0300

    u9

--- scripts.js
+++ scripts.js
@@ -14,31 +14,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     saveCard.addEventListener("click", function () {
-        if (nameDisplay.textContent.trim() !== "") {
-            const cardImage = document.getElementById("card-image");
-            const canvas = document.createElement("canvas");
-            const ctx = canvas.getContext("2d");
+  if (nameDisplay.textContent.trim() !== "") {
+    const cardImage = document.getElementById("card-image");
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-            canvas.width = cardImage.width;
-            canvas.height = cardImage.height;
+    canvas.width = cardImage.width;
+    canvas.height = cardImage.height;
 
-            ctx.drawImage(cardImage, 0, 0);
-            ctx.font = "bold 3em Arial";
-            ctx.fillStyle = "white";
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.shadowColor = "black";
-            ctx.shadowOffsetX = 2;
-            ctx.shadowOffsetY = 2;
-            ctx.shadowBlur = 4;
-            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height * 0.8);
+    ctx.drawImage(cardImage, 0, 0);
+    ctx.font = "bold 3em Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.shadowColor = "black";
+    ctx.shadowOffsetX = 2;
+    ctx.shadowOffsetY = 2;
+    ctx.shadowBlur = 4;
+    ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height * 0.8);
 
-            const link = document.createElement("a");
-            link.href = canvas.toDataURL("image/png");
-            link.download = "بطاقة_التهنئة.png";
-            link.click();
-        } else {
-            alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
-        }
-    });
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL("image/png");
+    link.download = "بطاقة_التهنئة.png";
+    link.click();
+  } else {
+    alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
+  }
 });
