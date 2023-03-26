4e5b5f55d019b038988a4e52309d9f7a36bf5dc6
Ibrahimmf <ibr1122@gmail.com>
2023-03-26 17:17:04 +0300

    chabge123

--- Archive/scripts.js
+++ Archive/scripts.js
@@ -3,26 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const submitName = document.getElementById("submit-name");
     const saveCard = document.getElementById("save-card");
     const nameDisplay = document.getElementById("name-display");
+    const generateCard = function () {
+    const name = nameInput.value;
+    if (name.trim() !== "") {
+        nameDisplay.textContent = name;
+        const cardText = document.getElementById("card-text");
+        cardText.style.display = "block";
+        const cardImage = document.getElementById("card-image");
+        cardImage.style.display = "block";
+    } else {
+        alert("الرجاء إدخال اسم قبل المتابعة.");
+    }
+};
 
-    submitName.addEventListener("click", function () {
-        const name = nameInput.value;
-        if (name.trim() !== "") {
-            nameDisplay.textContent = name;
-        } else {
-            alert("الرجاء إدخال اسم قبل المتابعة.");
-        }
-    });
 
-    saveCard.addEventListener("click", function () {
-if (nameDisplay.textContent.trim() !== "") {
-const cardImage = document.getElementById("card-image");
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
+   saveCard.addEventListener("click", function () {
+    if (nameDisplay.textContent.trim() !== "") {
+        const cardImage = document.getElementById("card-image");
+        const cardText = document.getElementById("card-text");
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
 
-        canvas.width = cardImage.width;
-        canvas.height = cardImage.height;
+        canvas.width = cardImage.offsetWidth;
+        canvas.height = cardImage.offsetHeight;
 
-        ctx.drawImage(cardImage, 0, 0);
+        ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
         ctx.font = "bold 3em Arial";
         ctx.fillStyle = "white";
         ctx.textAlign = "center";
@@ -33,10 +38,12 @@ const ctx = canvas.getContext("2d");
         ctx.shadowBlur = 4;
         ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height * 0.8);
 
-        const link = document.createElement("a");
-        link.href = canvas.toDataURL("image/png");
-        link.download = "بطاقة_التهنئة.png";
-        link.click();
+        canvas.toBlob(function (blob) {
+            const link = document.createElement("a");
+            link.href = window.URL.createObjectURL(blob);
+            link.download = "بطاقة_التهنئة.png";
+            link.click();
+        });
     } else {
         alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
     }
