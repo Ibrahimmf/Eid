eafa17ce23c68b49634fd9ce5beb0dc51513585c
Ibrahimmf <ibr1122@gmail.com>
2023-04-16 17:34:29 +0300

    تغيير الخط

--- scripts.js
+++ scripts.js
@@ -24,32 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
 	 
 	 generateCard();
  });
-    saveCard.addEventListener("click", function () {
-        if (nameDisplay.textContent.trim() !== "") {
-            const cardImage = document.getElementById("card-image");
-            const cardText = document.getElementById("card-text");
-            const canvas = document.createElement("canvas");
-            const ctx = canvas.getContext("2d");
+saveCard.addEventListener("click", function () {
+  if (nameDisplay.textContent.trim() !== "") {
+    const cardImage = document.getElementById("card-image");
+    const cardText = document.getElementById("card-text");
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-            canvas.width = cardImage.offsetWidth;
-            canvas.height = cardImage.offsetHeight;
+    canvas.width = cardImage.offsetWidth;
+    canvas.height = cardImage.offsetHeight;
 
-            ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
-	    ctx.font = "bold 1em Arial";
-	    ctx.fillStyle = "#835C3B";
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height / 2);
+    ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
+    ctx.font = "bold 20px Almarai, sans-serif";
+    ctx.fillStyle = "#835C3B"; // Brown color
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height / 2);
 
-
-            canvas.toBlob(function (blob) {
-                const link = document.createElement("a");
-                link.href = window.URL.createObjectURL(blob);
-                link.download = "بطاقة_التهنئة.png";
-                link.click();
-            });
-        } else {
-            alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
-        }
+    canvas.toBlob(function (blob) {
+      const link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+      link.download = "بطاقة_التهنئة.png";
+      link.click();
     });
+  } else {
+    alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
+  }
+});
+
 });
