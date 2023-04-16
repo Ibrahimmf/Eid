4ea65359b56c1d1a73383d22117421572047919f
Ibrahimmf <ibr1122@gmail.com>
2023-04-16 15:44:33 +0300

    ٩٩٠٩

--- scripts.js
+++ scripts.js
@@ -20,39 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
-submitName.addEventListener("click", function () {
-    generateCard();
+ submitName.addEventListener("click", function () {
+	 
+	 generateCard();
+ });
+    saveCard.addEventListener("click", function () {
+        if (nameDisplay.textContent.trim() !== "") {
+            const cardImage = document.getElementById("card-image");
+            const cardText = document.getElementById("card-text");
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            canvas.width = cardImage.offsetWidth;
+            canvas.height = cardImage.offsetHeight;
+
+            ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
+	    ctx.font = "bold 1em Arial";
+	    ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height / 2);
+
+
+            canvas.toBlob(function (blob) {
+                const link = document.createElement("a");
+                link.href = window.URL.createObjectURL(blob);
+                link.download = "بطاقة_التهنئة.png";
+                link.click();
+            });
+        } else {
+            alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
+        }
+    });
 });
-
-saveCard.addEventListener("click", function () {
-    if (nameDisplay.textContent.trim() !== "") {
-        const cardImage = document.getElementById("card-image");
-        const cardText = document.getElementById("card-text");
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-
-        canvas.width = cardImage.offsetWidth;
-        canvas.height = cardImage.offsetHeight;
-
-        ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
-        ctx.font = "20px Almarai, sans-serif";
-        ctx.fillStyle = "#61481b";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.shadowColor = "black";
-        ctx.shadowOffsetX = 2;
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 4;
-        ctx.fillText(nameDisplay.textContent, cardImage.offsetWidth / 2, cardImage.offsetHeight * 0.8);
-
-        canvas.toBlob(function (blob) {
-            const link = document.createElement("a");
-            link.href = window.URL.createObjectURL(blob);
-            link.download = "بطاقة_التهنئة.png";
-            link.click();
-        });
-    } else {
-        alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
-    }
-});
-
