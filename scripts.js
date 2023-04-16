c901125c20a5cc4d6da03c697ef1e706267015e0
Ibrahimmf <ibr1122@gmail.com>
2023-04-16 18:22:29 +0300

    new bottom

--- scripts.js
+++ scripts.js
@@ -24,39 +24,43 @@ document.addEventListener("DOMContentLoaded", function () {
 	 
 	 generateCard();
  });
-saveCard.addEventListener("click", function () {
-  if (nameDisplay.textContent.trim() !== "") {
-    const cardImage = document.getElementById("card-image");
-    const cardText = document.getElementById("card-text");
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
+	saveCard.addEventListener("click", function () {
+        if (nameDisplay.textContent.trim() !== "") {
+            const cardImage = document.getElementById("card-image");
+            const cardText = document.getElementById("card-text");
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
 
-    canvas.width = cardImage.offsetWidth;
-    canvas.height = cardImage.offsetHeight;
+            canvas.width = cardImage.offsetWidth;
+            canvas.height = cardImage.offsetHeight;
 
-    ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
-    ctx.font = "20px Almarai, sans-serif";
-    ctx.fillStyle = "#835C3B"; // Brown color
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height / 2);
+            ctx.drawImage(cardImage, 0, 0, canvas.width, canvas.height);
+	    ctx.font = "20px Almarai, sans-serif";
+            ctx.fillStyle = "#835C3B";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText(nameDisplay.textContent, canvas.width / 2, canvas.height / 2);
 
-    canvas.toBlob(function (blob) {
-  const link = document.createElement("a");
-  link.href = window.URL.createObjectURL(blob);
-  link.download = "بطاقة_التهنئة.png";
+            const shareButton = document.createElement("button");
+            shareButton.textContent = "مشاركة على الواتساب";
+            shareButton.addEventListener("click", function () {
+                const dataUrl = canvas.toDataURL();
+                const encodedData = encodeURIComponent(dataUrl);
+                const whatsappUrl = "whatsapp://send?text=" + encodedData;
+                window.location.href = whatsappUrl;
+            });
 
-  // إنشاء رابط مشاركة الواتساب
-  const whatsappLink = "https://api.whatsapp.com/send?text=تم%20إنشاء%20هذه%20البطاقة%20لك%20من%20موقع%20التهنئة.%20قم%20بتحميل%20الصورة:%20" + encodeURIComponent(link.href);
-  const whatsappBtn = document.createElement("a");
-  whatsappBtn.href = whatsappLink;
-  whatsappBtn.target = "_blank";
-  whatsappBtn.innerText = "مشاركة عبر الواتساب";
+            const cardContainer = document.getElementById("card-container");
+            cardContainer.appendChild(shareButton);
 
-  // إضافة الزر لصفحة الويب
-  const container = document.getElementById("container");
-  container.appendChild(whatsappBtn);
-});
+            canvas.toBlob(function (blob) {
+                const link = document.createElement("a");
+                link.href = window.URL.createObjectURL(blob);
+                link.download = "بطاقة_التهنئة.png";
+                link.click();
+    
+    });
+
 
   } else {
     alert("الرجاء إدخال اسم وعرض البطاقة قبل حفظها.");
